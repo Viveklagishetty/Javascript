8c9fbe98cfa59d691e67e01d2e74531d2973bd40
Viveklagishetty <vivekdattalagishetty@gmail.com>
2026-04-02 18:27:01 +0530

    js Task-2

--- script.js
+++ script.js
@@ -1,212 +1,212 @@
-// 1
-var a = 10;
-a = 20;
-var a = 30;
-console.log(a); // Output: 30
+// // 1
+// var a = 10;
+// a = 20;
+// var a = 30;
+// console.log(a); // Output: 30
 
-// 2
-var a = 5;
-var a = 15;
-console.log(a); // Output: 15
+// // 2
+// var a = 5;
+// var a = 15;
+// console.log(a); // Output: 15
 
-// 3
-var a = 1;
-a = 2;
-a = 3;
-console.log(a); // Output: 3
+// // 3
+// var a = 1;
+// a = 2;
+// a = 3;
+// console.log(a); // Output: 3
 
-// 4
-var a = 7;
-a = 14;
-var a = 21;
-a = 28;
-console.log(a); // Output: 28
+// // 4
+// var a = 7;
+// a = 14;
+// var a = 21;
+// a = 28;
+// console.log(a); // Output: 28
 
-// 5
-var a = 100;
-var a = 200;
-a = 300;
-console.log(a); // Output: 300
+// // 5
+// var a = 100;
+// var a = 200;
+// a = 300;
+// console.log(a); // Output: 300
 
-// 6
-var a = 9;
-console.log(a); // Output: 9
+// // 6
+// var a = 9;
+// console.log(a); // Output: 9
 
-// 7
-var a = 11;
-a = 22;
-console.log(a); // Output: 22
+// // 7
+// var a = 11;
+// a = 22;
+// console.log(a); // Output: 22
 
-// 8
-var a = 50;
-var a = 60;
-var a = 70;
-console.log(a); // Output: 70
+// // 8
+// var a = 50;
+// var a = 60;
+// var a = 70;
+// console.log(a); // Output: 70
 
-// 9
-var a = 2;
-a = 4;
-a = 6;
-var a = 8;
-console.log(a); // Output: 8
+// // 9
+// var a = 2;
+// a = 4;
+// a = 6;
+// var a = 8;
+// console.log(a); // Output: 8
 
-// 10
-var a = 99;
-a = 88;
-console.log(a); // Output: 88
-
-
-// 11
-let b = 10;
-b = 20;
-console.log(b); // Output: 20
-
-// 12
-let b = 5;
-b = 15;
-b = 25;
-console.log(b); // output: 25
-
-// 13
-let b = 1;
-console.log(b); // Output: 1
-
-// 14
-let b = 7;
-b = 14;
-console.log(b); // Output: 14
-
-// 15
-let b = 100;
-b = 200;
-b = 300;
-console.log(b); // Output: 300
-
-// 16
-let b = 9;
-let b = 18;
-console.log(b); // Output: SyntaxError: Identifier 'b' has already been declared
-
-// 17
-let b = 50;
-b = 60;
-let b = 70;
-console.log(b); // Output: SyntaxError: Identifier 'b' has already been declared
-
-// 18
-let b = 2;
-b = 4;
-console.log(b);// Output: 4
-
-// 19
-let b = 99;
-b = 88;
-b = 77;
-console.log(b); // Output: 77
-
-// 20
-let b = 11;
-b = 22;
-console.log(b);// Output: 22
+// // 10
+// var a = 99;
+// a = 88;
+// console.log(a); // Output: 88
 
 
-// 21
-const c = 10;
-console.log(c);// Output: 10
+// // 11
+// let b = 10;
+// b = 20;
+// console.log(b); // Output: 20
 
-// 22
-const c = 5;
-c = 15;
-console.log(c);// Output: TypeError: Assignment to constant variable.
+// // 12
+// let b = 5;
+// b = 15;
+// b = 25;
+// console.log(b); // output: 25
 
-// 23
-const c = 1;
-const c = 2;
-console.log(c);// Output: SyntaxError: Identifier 'c' has already been declared
+// // 13
+// let b = 1;
+// console.log(b); // Output: 1
 
-// 24
-const c = 7;
-console.log(c);// Output: 7
+// // 14
+// let b = 7;
+// b = 14;
+// console.log(b); // Output: 14
 
-// 25
-const c = 100;
-console.log(c);// Output: 100
+// // 15
+// let b = 100;
+// b = 200;
+// b = 300;
+// console.log(b); // Output: 300
 
-// 26
-const c = 50;
-c = 60;
-console.log(c);// Output: TypeError: Assignment to constant variable.
+// // 16
+// let b = 9;
+// let b = 18;
+// console.log(b); // Output: SyntaxError: Identifier 'b' has already been declared
 
-// 27
-const c = 9;
-console.log(c);// Output: 9
+// // 17
+// let b = 50;
+// b = 60;
+// let b = 70;
+// console.log(b); // Output: SyntaxError: Identifier 'b' has already been declared
 
-// 28
-const c = 20;
-const c = 30;
-console.log(c);// Output: SyntaxError: Identifier 'c' has already been declared
+// // 18
+// let b = 2;
+// b = 4;
+// console.log(b);// Output: 4
 
-// 29
-const c = 99;
-console.log(c);// Output: 99
+// // 19
+// let b = 99;
+// b = 88;
+// b = 77;
+// console.log(b); // Output: 77
 
-// 30
-const c = 11;
-c = 22;
-console.log(c);// Output: TypeError: Assignment to constant variable.
+// // 20
+// let b = 11;
+// b = 22;
+// console.log(b);// Output: 22
 
 
-// 31
-var a = 1;
-var a = 2;
-var a = 3;
-console.log(a);// Output: 3
+// // 21
+// const c = 10;
+// console.log(c);// Output: 10
 
-// 32
-var a = 10;
-a = 20;
-a = 30;
-var a = 40;
-console.log(a);// Output: 40
+// // 22
+// const c = 5;
+// c = 15;
+// console.log(c);// Output: TypeError: Assignment to constant variable.
 
-// 33
-var a = 5;
-a = 15;
-console.log(a);// Output: 15
+// // 23
+// const c = 1;
+// const c = 2;
+// console.log(c);// Output: SyntaxError: Identifier 'c' has already been declared
 
-// 34
-var a = 100;
-var a = 200;
-var a = 300;
-a = 400;
-console.log(a);// Output: 400
+// // 24
+// const c = 7;
+// console.log(c);// Output: 7
 
-// 35
-var a = 7;
-console.log(a);// Output: 7
+// // 25
+// const c = 100;
+// console.log(c);// Output: 100
 
-// 36
-let b = 3;
-b = 6;
-b = 9;
-console.log(b);// Output: 9
+// // 26
+// const c = 50;
+// c = 60;
+// console.log(c);// Output: TypeError: Assignment to constant variable.
 
-// 37
-let b = 12;
-let b = 24;
-console.log(b);// Output: SyntaxError: Identifier 'b' has already been declared
+// // 27
+// const c = 9;
+// console.log(c);// Output: 9
 
-// 38
-let b = 8;
-b = 16;
-console.log(b);// Output: 16
+// // 28
+// const c = 20;
+// const c = 30;
+// console.log(c);// Output: SyntaxError: Identifier 'c' has already been declared
 
-// 39
-const c = 2;
-console.log(c);// Output: 2
+// // 29
+// const c = 99;
+// console.log(c);// Output: 99
 
-// 40
-const c = 4;
-c = 8;
-Console.log(c);// Output: TypeError: Assignment to constant variable.
+// // 30
+// const c = 11;
+// c = 22;
+// console.log(c);// Output: TypeError: Assignment to constant variable.
+
+
+// // 31
+// var a = 1;
+// var a = 2;
+// var a = 3;
+// console.log(a);// Output: 3
+
+// // 32
+// var a = 10;
+// a = 20;
+// a = 30;
+// var a = 40;
+// console.log(a);// Output: 40
+
+// // 33
+// var a = 5;
+// a = 15;
+// console.log(a);// Output: 15
+
+// // 34
+// var a = 100;
+// var a = 200;
+// var a = 300;
+// a = 400;
+// console.log(a);// Output: 400
+
+// // 35
+// var a = 7;
+// console.log(a);// Output: 7
+
+// // 36
+// let b = 3;
+// b = 6;
+// b = 9;
+// console.log(b);// Output: 9
+
+// // 37
+// let b = 12;
+// let b = 24;
+// console.log(b);// Output: SyntaxError: Identifier 'b' has already been declared
+
+// // 38
+// let b = 8;
+// b = 16;
+// console.log(b);// Output: 16
+
+// // 39
+// const c = 2;
+// console.log(c);// Output: 2
+
+// // 40
+// const c = 4;
+// c = 8;
+// Console.log(c);// Output: TypeError: Assignment to constant variable.
 
